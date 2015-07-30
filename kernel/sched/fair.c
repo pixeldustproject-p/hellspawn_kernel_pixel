219b7d9082c865e741969dcd99cba58e272a0dec
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-07-30 16:53:30 +0100

    sched: EAS & 'single cpu per cluster'/cpu hotplug interoperability
    For Energy-Aware Scheduling (EAS) to work properly, even in the
    case that there is only one cpu per cluster or that cpus are hot-plugged
    out, the Energy Model (EM) data on all energy-aware sched domains (sd)
    has to be present for all online cpus.
    
    Mainline sd hierarchy setup code will remove sd's which are not useful
    for task scheduling e.g. in the following situations:
    
    1. Only 1 cpu is/remains in one cluster of a multi cluster system.
    
       This remaining cpu only has DIE and no MC sd.
    
    2. A complete cluster in a two cluster system is hot-plugged out.
    
       The cpus of the remaining cluster only have MC and no DIE sd.
    
    To make sure that all online cpus keep all their energy-aware sd's,
    the sd degenerate functionality has been changed to not free a sd if
    its first sched group (sg) contains EM data in case:
    
    1. There is only 1 cpu left in the sd.
    
    2. There have to be at least 2 sg's if certain sd flags are set.
    
    Instead of freeing such a sd it now clears only its SD_LOAD_BALANCE
    flag. This will make sure that the EAS functionality will always see
    all energy-aware sd's for all online cpus.
    
    It will introduce a tiny performance degradation for operations on
    affected cpus since the hot-path macro for_each_domain() has to deal
    with sd's not contributing to task scheduling at all now.
    
    In most cases the exisiting code makes sure that task scheduling is not
    invoked on a sd with !SD_LOAD_BALANCE.
    
    However, a small change is necessary in update_sd_lb_stats() to make
    sure that sd->parent is only initialized to !NULL in case the parent sd
    contains more than 1 sg.
    
    The handling of newidle decay values before the SD_LOAD_BALANCE check in
    rebalance_domains() stays unchanged.
    
    Test (w/ CONFIG_SCHED_DEBUG):
    
    JUNO r0 default system:
    
    $ cat /proc/cpuinfo | grep "^CPU part"
    CPU part        : 0xd03
    CPU part        : 0xd07
    CPU part        : 0xd07
    CPU part        : 0xd03
    CPU part        : 0xd03
    CPU part        : 0xd03
    
    SD names and flags:
    
    $ cat /proc/sys/kernel/sched_domain/cpu*/domain*/name
    MC
    DIE
    MC
    DIE
    MC
    DIE
    MC
    DIE
    MC
    DIE
    MC
    DIE
    
    $ printf "%x\n" `cat /proc/sys/kernel/sched_domain/cpu*/domain*/flags`
    832f
    102f
    832f
    102f
    832f
    102f
    832f
    102f
    832f
    102f
    832f
    102f
    
    Test 1: Hotplug-out one A57 (CPU part 0xd07) cpu:
    
    $ echo 0 > /sys/devices/system/cpu/cpu1/online
    
    $ cat /proc/cpuinfo | grep "^CPU part"
    CPU part        : 0xd03
    CPU part        : 0xd07
    CPU part        : 0xd03
    CPU part        : 0xd03
    CPU part        : 0xd03
    
    SD names and flags for remaining A57 (cpu2) cpu:
    
    $ cat /proc/sys/kernel/sched_domain/cpu2/domain*/name
    MC
    DIE
    
    $ printf "%x\n" `cat /proc/sys/kernel/sched_domain/cpu2/domain*/flags`
    832e <-- MC SD with !SD_LOAD_BALANCE
    102f
    
    Test 2: Hotplug-out the entire A57 cluster:
    
    $ echo 0 > /sys/devices/system/cpu/cpu1/online
    $ echo 0 > /sys/devices/system/cpu/cpu2/online
    
    $ cat /proc/cpuinfo | grep "^CPU part"
    CPU part        : 0xd03
    CPU part        : 0xd03
    CPU part        : 0xd03
    CPU part        : 0xd03
    
    SD names and flags for the remaining A53 (CPU part 0xd03) cluster:
    
    $ cat /proc/sys/kernel/sched_domain/cpu*/domain*/name
    MC
    DIE
    MC
    DIE
    MC
    DIE
    MC
    DIE
    
    $ printf "%x\n" `cat /proc/sys/kernel/sched_domain/cpu*/domain*/flags`
    832f
    102e <-- DIE SD with !SD_LOAD_BALANCE
    832f
    102e
    832f
    102e
    832f
    102e
    
    Change-Id: Iaa676b2a59d826d1f909921c3bc6365e71c6690e
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7741,6 +7741,9 @@ static inline enum fbq_type fbq_classify_rq(struct rq *rq)
 }
 #endif /* CONFIG_NUMA_BALANCING */
 
+#define lb_sd_parent(sd) \
+	(sd->parent && sd->parent->groups != sd->parent->groups->next)
+
 /**
  * update_sd_lb_stats - Update sched_domain's statistics for load balancing.
  * @env: The load balancing environment.
@@ -7823,7 +7826,7 @@ next_group:
 
 	env->src_grp_nr_running = sds->busiest_stat.sum_nr_running;
 
-	if (!env->sd->parent) {
+	if (!lb_sd_parent(env->sd)) {
 		/* update overload indicator if we are at root domain */
 		if (env->dst_rq->rd->overload != overload)
 			env->dst_rq->rd->overload = overload;
@@ -8331,7 +8334,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 			int *continue_balancing)
 {
 	int ld_moved, cur_ld_moved, active_balance = 0;
-	struct sched_domain *sd_parent = sd->parent;
+	struct sched_domain *sd_parent = lb_sd_parent(sd) ? sd->parent : NULL;
 	struct sched_group *group;
 	struct rq *busiest;
 	unsigned long flags;
